36130711f1b2e5ff00cc7378574c50d68a233568
Shannon <763461297@qq.com>
2016-12-10 20:51:34 +0800

    login fixed

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -2,7 +2,6 @@
 namespace app\index\controller;
 
 use think\Controller;
-use think\View;
 use think\Request;
 use think\Session;
 use think\Db;
@@ -12,35 +11,42 @@ class Login extends Controller
     public function index(Request $request)
     {
         if (!Session::has('user', 'scheduler')) {
+        	$this->assign([
+        			'title' => '用户登录',
+        	]);
             return $this->fetch();
         } else {
-            $this->redirect('index');
+            $this->redirect('index/index/index');
         }
     }
     
     public function verify(Request $request) {
-        if ($request->param('user') && $request->param('password')) {
-            $user = Db::name('admin')->where(['user' => $request->param('user')])->find();
-            if ($user == '') {
-                return $this->getAjaxResp("用户名或密码错误!", false);
-            } else {
-                if ($user['passwd'] == $request->param('password')) {
-                    $user['logintime'] = time();
-                    $user['loginip'] = $request->ip();
-                    Db::name('admin')->where(['user' => $request->param('user')])->update($user);
-                    session('user', $request->param('user'), 'scheduler');
-                    return json([
-                        'success' => true,
-                        'url' => url('index/index/index'),
-                        'msg' => 'success',
-                    ]);
-                } else {
-                    return $this->getAjaxResp("用户名或密码错误!", false);
-                }
-            }
-        } else {
-            return $this->getAjaxResp("请输入完整信息!", false);
-        }
+    	if($request->isAjax()) {
+	        if ($request->param('user') && $request->param('password')) {
+	            $user = Db::name('admin')->where(['user' => $request->param('user')])->find();
+	            if ($user == '') {
+	                return $this->getAjaxResp("用户名或密码错误!", false);
+	            } else {
+	                if ($user['passwd'] == $request->param('password')) {
+	                    $user['logintime'] = time();
+	                    $user['loginip'] = $request->ip(0);
+	                    Db::name('admin')->where(['user' => $request->param('user')])->update($user);
+	                    session('user', $request->param('user'), 'scheduler');
+	                    return json([
+	                        'success' => true,
+	                        'url' => url('index/index/index'),
+	                        'msg' => 'success',
+	                    ]);
+	                } else {
+	                    return $this->getAjaxResp("用户名或密码错误!", false);
+	                }
+	            }
+	        } else {
+	            return $this->getAjaxResp("请输入完整信息!", false);
+	        }
+    	} else {
+    		$this->error();
+    	}
     }
 
     public function out() {
